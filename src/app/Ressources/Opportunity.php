5be4aa43dce97078f09002decd437f5d7c105962
B1nj <postmaster@b1nj.fr>
2021-10-13 08:56:14 -0400

    add acompte

--- src/app/Ressources/Opportunity.php
+++ src/app/Ressources/Opportunity.php
@@ -15,17 +15,19 @@ class Opportunity extends Ressource
      * @param $code_societe_source
      * @param null|string $transaction_reference
      * @param bool $is_devis
+     * @param null|float $acompte
      * @return string
      * @throws SalesforceException
      * @throws GuzzleException
      */
-    public function post($client_reference_salesforce, $vin, $code_societe_source, $transaction_reference = null, $is_devis = false)
+    public function post($client_reference_salesforce, $vin, $code_societe_source, $is_devis = true, $transaction_reference = null, $acompte = null)
     {
         $datas = [
             "ID_SALESFORCE" => $client_reference_salesforce,
             "StageName" => $is_devis ? "Quotation" : 'Closed Won',
             "VIN_VEHICULE" => $vin.'_'.str_pad($code_societe_source, 3, '0', STR_PAD_LEFT),
             "TRANSACTION_PAYBOX" => $transaction_reference,
+            "MONTANT_ACOMPTE" => $acompte,
         ];
 
         $response = $this->api->post('apexrest/OpportunityManager/v1.0', [
